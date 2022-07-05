8bbdae1ca11ad5d64d72804835d0b81785d2b216
hemantgabra <30734567+hemantgabra@users.noreply.github.com>
2022-07-05 23:09:41 +0530

    SSR

---
+++ components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <nav>
+      <ul class="topNav">
+        <li><nuxt-link to="/">Home</nuxt-link></li>
+        <li><nuxt-link to="/">Services</nuxt-link></li>
+        <li><nuxt-link to="/">Products</nuxt-link></li>
+        <li><nuxt-link to="/">Career</nuxt-link></li>
+        <li><nuxt-link to="/">Contact</nuxt-link></li>
+      </ul>
+    </nav>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,44 +1,20 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">first-nuxt-app</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+    <div class="row">
+      <div class="col-6"><h1>This is the Home Page</h1></div>
+      <div class="col-6 mt-3"><Header></Header></div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+import Header from '../components/Header.vue'
+export default {
+  components: Header,
+}
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
 .title {
   font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
@@ -59,5 +35,12 @@ export default {}
 
 .links {
   padding-top: 15px;
+}
+ul.topNav {
+  list-style: none;
+  display: flex;
+}
+ul.topNav li {
+  padding: 0 20px;
 }
 </style>
